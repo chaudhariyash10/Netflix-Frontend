2428ab36694468e413a51c895491db4ddfa8d216
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-01-23 11:47:18 +0530

    Fixed the banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import axios from "./axios";
 import React, { useState, useEffect } from "react";
 import requests from "./requests";
+import "./Banner.css";
 function Banner() {
   const [movie, setMovie] = useState([]);
 
@@ -22,9 +23,19 @@ function Banner() {
   console.log(movie);
   return (
     <header>
-      <div className="banner">
-        <div className="banner-contents">
-          <h1>{movie?.name || movie?.title || movie?.original_name}</h1>
+      <div
+        className="banner background__poster"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner_title">{movie?.name || movie?.title || movie?.original_name}</h1>
+          <div className="banner_buttons">
+            <button className="banner-button">Play</button>
+            <button className="banner-button">My List</button>
+          </div>
+          <h1 className="banner_description">{movie?.overview}</h1>
         </div>
       </div>
     </header>

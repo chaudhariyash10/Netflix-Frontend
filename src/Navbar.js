4edfa1509a5b25cc7d66b5d64f37c7c4731884d1
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-01-25 21:05:54 +0530

    Added Navbar to homepage

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scroll > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className={`nav ${show && "nav__black"}`}>
+        <img
+          className="nav__avatar"
+          src={
+            "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          }
+          alt="profile"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
